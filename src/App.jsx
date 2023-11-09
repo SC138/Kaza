b07f7a1fb2285cc2d883d73767b612bc0a55301d
Thomas Chevron <chevron.thomas33@gmail.com>
2023-11-09 19:24:38 +0100

    new project with Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./Pages/Home/Home.jsx";
+// import { About } from "./Pages/About";
+// import { ApartmentRental } from "./Pages/ApartmentRental";
+// import { Error404 } from "./Pages/Error404";
+
+function App() {
+  return (
+    <>
+      <div>hello</div>
+    </>
+  );
+}
+
+export default App;
