3765def0124407aba8b8cddca94a2a98c64a4a6e
Thomas Chevron <chevron.thomas33@gmail.com>
2023-11-27 17:52:16 +0100

    fixe rating

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,25 +1,23 @@
 import s from "./style.module.css";
-import { StarFill, Star as StarHalf } from "react-bootstrap-icons";
+import { StarFill, Star } from "react-bootstrap-icons";
 import PropTypes from "prop-types";
 
 // Définit le composant Rating pour afficher les étoiles.
 export function Rating({ rating }) {
   const starList = [];
 
-  // Calcule le nombre d'étoiles pleines, demi-étoiles et vides.
+  // Calcule le nombre d'étoiles pleines et vides.
   const starFillCount = Math.floor(rating);
-  const hasStarHalf = rating - starFillCount >= 0.5;
-  const starEmptyCount = 5 - starFillCount - (hasStarHalf ? 1 : 0);
+  const starEmptyCount = 5 - starFillCount;
 
-  // Boucle pour ajouter les étoiles pleines, demi-étoiles et vides dans le tableau starList.
+  // Boucle pour ajouter les étoiles pleines dans le tableau starList.
   for (let i = 0; i < starFillCount; i++) {
     starList.push(<StarFill key={"star-fill" + i} className={s.star} />);
   }
-  if (hasStarHalf) {
-    starList.push(<StarHalf key={"star-half"} className={s.star} />);
-  }
+
+  // Boucle pour ajouter les étoiles vides dans le tableau starList.
   for (let i = 0; i < starEmptyCount; i++) {
-    starList.push(<StarFill key={"star-empty" + i} className={s.starEmpty} />);
+    starList.push(<Star key={"star-empty" + i} className={s.starEmpty} />);
   }
 
   // Rendu du composant Rating.
